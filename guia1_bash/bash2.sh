c0910d43368e098c57328e6786227db7451c4e2f
Agus <agcordoba@fi.uba.ar>
2026-03-24 01:15:24 +0000

    Insertar nombre y apellido del alumno

--- guia1_bash/bash2.sh
+++ guia1_bash/bash2.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Crear un script de Bash que reciba dos números e imprima por pantalla su suma.
 
-num1=10
-num2=32
+num1=$1
+num2=$2
 suma=$((num1+num2))
 echo "la suma de $num1 y $num2 es = $suma"
-
-chmod +x guia1_bash/bash2.sh
